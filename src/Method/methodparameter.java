5d3635fcfe6235ac71a8b15ab2df190c0ed62d81
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-17 23:27:38 +0530

    Create methodparameter.java

---
+++ src/Method/methodparameter.java
@@ -0,0 +1,14 @@
+public class Main {
+  static void myMethod(String fname) {
+    System.out.println(fname + " prajapat");
+  }
+
+  public static void main(String[] args) {
+    myMethod("bharat");
+    myMethod("ajay");
+    myMethod("kapil");
+  }
+}
+// bharat prajapat
+// ajay prajapat
+// kapil prajapat
